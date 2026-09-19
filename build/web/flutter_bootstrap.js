20ad784551419d00e77abfd906341b13ae54f0f9
Sławomir Grelich <slawomirgrelich@gmail.com>
2026-09-19 22:10:35 +0200

    Update GitHub Pages web build

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"af7e796e161ae0bb1ff0758c71a7105418bd9d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4208189638" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3283184752" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
