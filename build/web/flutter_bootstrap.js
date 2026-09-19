bc33dcd0f9e74c4ba40d4ad73f9a782ec8ab414b
Sławomir Grelich <slawomirgrelich@gmail.com>
2026-09-19 23:07:08 +0200

    Update expanded web build

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"af7e796e161ae0bb1ff0758c71a7105418bd9d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2932893736" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2161988347" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
